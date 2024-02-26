06da2f1280e587a0b1b3ea2652dd422303cb3603
unknown <enelry@code.edu.az>
2024-02-26 22:33:14 +0400

    added search mathod

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ async function GetMovies(){
     return data;
 }
 window.onload=async ()=>{
+
     let movies=await GetMovies();
     let parent=document.getElementById("main");
-    movies.forEach(movie => {
+    movies.forEach((movie) => {
        parent.innerHTML+=`<div class="col-3">
                     <div class="card" style="width:16.5rem; height: 42rem; margin:10px;">
                         <img src="${movie["image"]["medium"]}" class="card-img-top" alt="...">
@@ -16,10 +17,10 @@ window.onload=async ()=>{
                         </div>
                         <ul class="list-group list-group-flush">
                           <li class="list-group-item">IMDB Rating:${movie["rating"]["average"]}</li>
-                          <li class="list-group-item">Genre:${movie["genres"]}</li>
+                          <li class="list-group-item">Genre:${movie["genres"][0]}</li>
                           <li class="list-group-item">Language: ${movie["language"]}</li>
                         </ul>
-                        <div class="card-body" style="justify-content:center;align-content:space-around;">
+                        <div class="card-body">
                         <a href="#" class="btn btn-success btn-sm">Go To Website</a> 
                         <a href="#" class="btn btn-primary btn-sm">Go To Detail</a>
                         </div>
@@ -27,3 +28,36 @@ window.onload=async ()=>{
                 </div>`
     });
 }
+
+// document.querySelector("#search-input").addEventListener("input", function() {
+//   var searchText = this.value.trim();
+//   console.log(searchText);
+//   let movies = GetMovies();
+//   let parent = document.getElementById("main");
+//   parent.innerHTML="";
+//   let htmlContent = '';
+
+//   movies.forEach((movie) => {
+//     if (movie.name.toLowerCase().includes(searchText.toLowerCase())){
+//       htmlContent +=`<div class="col-3">
+//                  <div class="card" style="width:16.5rem; height: 42rem; margin:10px;">
+//                      <img src="${movie["image"]["medium"]}" class="card-img-top" alt="...">
+//                      <div class="card-body">
+//                        <h5 class="card-title">${movie["name"]}</h5>
+//                        <p class="card-text">Premiere:${movie["premiered"]}</p>
+//                      </div>
+//                      <ul class="list-group list-group-flush">
+//                        <li class="list-group-item">IMDB Rating:${movie["rating"]["average"]}</li>
+//                        <li class="list-group-item">Genre:${movie["genres"][0]}</li>
+//                        <li class="list-group-item">Language: ${movie["language"]}</li>
+//                      </ul>
+//                      <div class="card-body">
+//                      <a href="#" class="btn btn-success btn-sm">Go To Website</a> 
+//                      <a href="#" class="btn btn-primary btn-sm">Go To Detail</a>
+//                      </div>
+//                  </div>
+//              </div>`
+//              parent.innerHTML = htmlContent;
+//     }
+//   });
+//  });
