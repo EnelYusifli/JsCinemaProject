cc278386c4f87285b2317fbc3d98a9d0ba36436e
unknown <enelry@code.edu.az>
2024-02-28 11:11:28 +0400

    added noMoviesFound page

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ async function paginateAndDisplayMovies(movies) {
         const movie = items[i];
         const movieHTML = `
         <div class="col-3">
-        <div class="card" style="width: 16.5rem; height: 42rem; margin: 10px;">
+        <div class="card" style="min-width: 16.5rem; min-height: 42rem; margin: 10px;">
             <img src="${movie["image"]["medium"]}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${movie["name"]}</h5>
@@ -30,7 +30,7 @@ async function paginateAndDisplayMovies(movies) {
                 <li class="list-group-item">Genre: ${movie["genres"][0]}</li>
                 <li class="list-group-item">Language: ${movie["language"]}</li>
             </ul>
-            <div class="card-body d-flex justify-content-center"> <!-- Center align buttons -->
+            <div class="card-body d-flex justify-content-center">
                 <a href="#" class="btn btn-success btn-sm m-2" onclick="window.location.href='${movie["officialSite"]}'; return false;">Go To Website</a>
                 <a href="#" class="btn btn-primary btn-sm m-2" onclick="window.location.href='detail.html?id=${movie["id"]}'; return false;">Go To Detail</a>
             </div>
@@ -104,18 +104,15 @@ form.onsubmit=function(e) {
     alert_length.classList.remove("d-none");
   } else{
     btn.value="Go Back To HomePage"
+    input.style.display="none";
     btn.onclick=function() {
       window.location.href = 'index.html';
       input.value="";
     }
   }
-  setTimeout(function(){
-    alert_empty.classList.add("d-none")
-    alert_length.classList.add("d-none");
-  },2000)
 }
 
-form.onblur=function() {
+input.onblur=function() {
     alert_empty.classList.add("d-none")
     alert_length.classList.add("d-none");
 }
@@ -135,10 +132,6 @@ input.onkeyup=function () {
     alert_empty.classList.add("d-none");
     alert_length.classList.add("d-none");
   }
-  setTimeout(function(){
-    alert_empty.classList.add("d-none")
-    alert_length.classList.add("d-none");
-  },2000)
 }
 
 input.addEventListener("keyup", async function() {
@@ -151,8 +144,10 @@ input.addEventListener("keyup", async function() {
     let movies = await GetMovies();
     const parent = document.querySelector("#main");
       parent.innerHTML = "";
+      let moviesFound = false;
     movies.forEach((movie) => {
       if (movie.name.toLowerCase().includes(searchText)) {
+        moviesFound = true;
         const movieHTML = `
           <div class="col-3">
             <div class="card" style="width:16.5rem; height: 42rem; margin:10px;">
@@ -166,17 +161,24 @@ input.addEventListener("keyup", async function() {
                 <li class="list-group-item">Genre: ${movie.genres[0]}</li>
                 <li class="list-group-item">Language: ${movie.language}</li>
               </ul>
-              <div class="card-body">
-              <a href="#" class="btn btn-success btn-sm"  onclick="window.location.href='${movie["officialSite"]}'; return false;">Go To Website</a>
-              <a href="#" class="btn btn-primary btn-sm"  onclick="window.location.href='detail.html?id=${movie["id"]}'; return false;">Go To Detail</a>
+              <div class="card-body d-flex justify-content-center">
+              <a href="#" class="btn btn-success btn-sm m-2"  onclick="window.location.href='${movie["officialSite"]}'; return false;">Go To Website</a>
+              <a href="#" class="btn btn-primary btn-sm m-2"  onclick="window.location.href='detail.html?id=${movie["id"]}'; return false;">Go To Detail</a>
               </div>
             </div>
           </div>`;
           parent.innerHTML += movieHTML;
    }
-       const pagination = document.querySelector(paginationContainer);
-      pagination.innerHTML = "";
-    });
+   const pagination = document.querySelector(paginationContainer);
+   pagination.innerHTML = "";
+  });
+  if (!moviesFound) {
+       parent.innerHTML=`<div class="no-movies-container">
+       <p class="no-movies-message">No Movies Found</p>
+       </div>
+       `
+   return;
+  }
   } catch (error) {
     console.error('Error fetching movies:', error);
   }
